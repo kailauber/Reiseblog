c26b81e7b49ae575321b50aafcfc8ddaf393ce5f
Kai Lauber <kailauber@outlook.com>
2023-06-15 14:11:55 +0200

    bilderretusche

---
+++ media/kreta/images/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create the directories if they do not exist
+mkdir -p small
+mkdir -p medium
+mkdir -p large
+
+# Scale and move images to respective directories
+for j in *.JPG
+do
+    filename=$(basename -- "$j")
+    ffmpeg.exe -i "$j" -vf scale=800:-1 "small/Small-${filename}"
+    ffmpeg.exe -i "$j" -vf scale=1200:-1 "medium/Medium-${filename}"
+    ffmpeg.exe -i "$j" -vf scale=1500:-1 "large/Large-${filename}"
+done
